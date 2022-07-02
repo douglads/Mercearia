1b46cb4c3e759d0a68930ef02f7f51731b4fdae7
douglas <douglasfm1995@gmail.com>
2022-07-02 18:12:15 -0300

    Adicionado método para criptografar senhas em md5

--- Mercearia/Entrar.cs
+++ Mercearia/Entrar.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace Mercearia
 {
     public partial class Entrar : Form
@@ -8,19 +10,25 @@ namespace Mercearia
         }
         private void btnEntrar_Click(object sender, EventArgs e)
         {
-            if (tbUsuario.Text == "" ||tbSenha.Text == "")
-            {
-                lblEntrarInvalido.Text = "Campos em branco";
-            }
-            else if(tbUsuario.Text == "Douglas" && tbSenha.Text == "Douglas")
-            {
-                lblEntrarInvalido.ForeColor = System.Drawing.Color.Green;
-                lblEntrarInvalido.Text = ("Bem vindo " + tbUsuario.Text);
-            }
-            else
-            {
-                lblEntrarInvalido.Text = "Usuário ou senha inválidos";
-            }
+            var hashSenha = Program.sha256encrypt(tbSenha.Text);
+
+            if (tbUsuario.Text == String.Empty || tbSenha.Text == String.Empty)
+                {
+                    lblEntrarInvalido.Text = "Campos em branco";
+                }
+                else if (tbUsuario.Text == "Douglads" && hashSenha == "D4460C6D9AA2BF8D2E6D91F392B95D3663D7435B8EF00B53643DC595E1DD68CF")
+                {
+                    lblEntrarInvalido.ForeColor = System.Drawing.Color.Green;
+                    lblEntrarInvalido.Text = ("Bem vindo " + tbUsuario.Text);
+                }
+                else if (tbUsuario.Text != String.Empty && tbSenha.Text.Length < 8)
+                {
+                    lblEntrarInvalido.Text = ("A senha deve ter mais de 8 caracteres");
+                }
+                else
+                {
+                    lblEntrarInvalido.Text = "Usuário ou senha inválidos";
+                }
         }
     }
 }
--- Mercearia/Program.cs
+++ Mercearia/Program.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Security.Cryptography;
+
 namespace Mercearia
 {
     internal static class Program
@@ -12,6 +15,23 @@ namespace Mercearia
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Entrar());
+        }
+
+        public static string sha256encrypt(string frase)
+        {
+            UTF8Encoding encoder = new UTF8Encoding();
+            SHA256Managed sha256hasher = new SHA256Managed();
+            byte[] hashedDataBytes = sha256hasher.ComputeHash(encoder.GetBytes(frase));
+            return byteArrayToString(hashedDataBytes);
+        }
+        public static string byteArrayToString(byte[] inputArray)
+        {
+            StringBuilder output = new StringBuilder("");
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                output.Append(inputArray[i].ToString("X2"));
+            }
+            return output.ToString();
         }
     }
 }
